778706f94ad099946ad5899f06b80c3b7ee57342
lauratomkins <lauratomkins@users.noreply.github.com>
2022-10-11 19:40:21 +0000

    deploy: 34fb9e3f671c592e652ff8a4b9f37e7f60847c76

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.6.post232',
+    VERSION: '1.11.6.post245',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
